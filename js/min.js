44d0117d16af44949e357a8effebaa8f43870463
Alisher3344 <alisher3344ali@gmail.com>
2024-07-15 17:44:08 +0500

    first commit

---
+++ js/min.js
@@ -0,0 +1,225 @@
+// // BEGIN1
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonnini kiriting");
+// let c = a * b;
+// let d = 2 * (a + b);
+
+// console.log("a = ", a);
+// console.log("b = ", b);
+// console.log("S = a * b = ", c);
+// console.log("P = 2 * (a + b) = ",d);
+
+// // BEGIN2
+// let a = 3.14;
+// let b = +prompt("diametrni kiriting");
+// let c = a * b;
+
+// console.log("L = ",c);
+
+// // BEGIN3
+// let a = +prompt("kubinng tomonini kiriting");
+// let A = a ** 3;
+// let b = a ** 2 * 6;
+
+// console.log("a = ", a);
+// console.log("v = a ^ 3 =", A);
+// console.log("S = 6 * a ^ 2 = ", b);
+
+// // BEGIN4
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni kiriting");
+// let c = +prompt("c ni kiriting");
+// let d = a * b * c;
+// let e = 2 * (a * b + b * c + a * c);
+
+// console.log("a = ", a);
+// console.log("b = ", b);
+// console.log("c = ", c);
+// console.log("V = a * b * c =", d);
+// console.log("S = 2 * (a * b + b * c + a * c) = ", e);
+
+// // BEGIN5
+// let a = +prompt ("a ni kiriting");
+// let b = +prompt ("b ni kiriting");
+// let c = (a + b) / 2;
+
+// console.log("a = ",a);
+// console.log("b = ",b);
+// console.log("M = (a + b) / 2 =",c);
+
+// // BEGIN6
+// let a = +prompt("a tominini kiriting");
+// let b = +prompt("b tominini kiriting");
+// let A = a ** 2;
+// let B = b ** 2;
+// let c = (A + B) ** (1 / 2);
+// let d = a + b + c;
+
+// console.log("gipatinuzasi = ",c);
+// console.log("p = a + b + c = ",d);
+
+// // BEGIN7
+// let a = 10 * 3.14;
+// let b = 1 * 3.14;
+// let e = a - b;
+// console.log("S = ",e);
+
+// // begin8
+// let R = +prompt("Radius");
+// const pi = 3.14;
+// let L = 2 * pi * R;
+// let S = pi * R ** 2;
+// console.log("L: ", L);
+// console.log("S: ", S);
+
+// // BEGIN9;
+// let x = +prompt("x1ni kiriting");
+// let y = +prompt("y1ni kiriting");
+// let X = +prompt("x2ni kiriting");
+// let Y = +prompt("y2ni kiriting");
+// let a = (x - X) ** 2 + (y - Y) ** 2;
+// let b = a ** (1 / 2);
+// console.log(b);
+
+// // BEGIN10
+// let a = +prompt("a =");
+// let b = +prompt("b =");
+// let c = +prompt("c =");
+
+// let d = a;
+// let e = b;
+
+// a = c;
+// b = e;
+// c = d;
+
+// console.log("qiymatlar almashtirildi!");
+// console.log("a = ",a);
+// console.log("b = ", b);
+// console.log("c = ", c);
+
+// // BEGIN11
+// let a=+prompt("x = ");
+// let b=4*(a-3)**6-7*(a-3)**3+2;
+// console.log("Y = ",b);
+
+// // BEGIN12
+// let a = +prompt("sonni kiriting");
+// let b = a ** 3;
+// let c = a ** 6;
+// let d = a ** 9;
+// let e = a ** 15;
+
+// console.log("^3 = ", b);
+// console.log("^6 = ", c);
+// console.log("^9 = ", d);
+// console.log("^15 = ", e);
+
+// // BEGIN13
+// let a = +prompt("farengeytni kiriting");
+// let b = (a - 32) * (5 / 9);
+// console.log("gradus selsiy = ", b);
+
+// // BEGIN14
+// let a = +prompt("necha kg shokolad");
+// let b = +prompt("necha kg konfet");
+// let c = a * 35000;
+// let d = b * 20000;
+// let e = c - d;
+// console.log("shokalad konfetdan",e ,"so'm arzon");
+
+// Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla fugiat ipsam ullam voluptate veritatis vel maxime ab nam voluptatum omnis a quaerat incidunt enim, laborum reprehenderit commodi. Quis, minus eligendi!
+
+// // BEGIN15
+// let a = +prompt("necha kg olma");
+// let b = +prompt("necha kg yashil-olma");
+// let c = a * 7000;
+// let d = b * 10000;
+// let e = c + d;
+// console.log(e ,"so'm xarit qilgan");
+
+// // INTEGER1
+// let abc = +prompt("uch xonali son kiriting");
+// let bc = abc % 100;
+// let a = (abc - bc) / 100;
+// let B = bc % 10;
+// let A = (bc - B) / 10;
+// console.log("Kiritilgan son = ",abc);
+// console.log("O'nliklar xonasi",A);
+// console.log("Birliklar xonasi",B);
+
+// // INTEGER2
+// let abc = +prompt("uch xonali son kiriting");
+// let ab = abc % 100;
+// let a = (abc - ab) / 100;
+// let b = (abc - a) % 10;
+// let c = ab % 10;
+// let y = a + b + c;
+// console.log("kiritilgan son = ",abc);
+// console.log("yuzlar xonasi = ",a);
+// console.log("o'nlar xonasi = ",b);
+// console.log("birlar xonasi = ",c);
+// console.log("sonlarning yig'indisi = ",y);
+
+// // INTEGER3
+// let abc = +prompt("uch xonali son kiriting");
+// let ab = abc % 100;
+// let a = (abc - ab) / 100;
+// let b = (abc - a) % 10;
+// let c = ab % 10;
+// console.log(a,b,c);
+// console.log(c,b,a);
+
+// // INTEGER4
+//  abc = +prompt("uch xonali son kiriting");
+// let ab = abc % 100;
+// let a = (abc - ab) / 100;
+// let b = (abc - a) % 10;
+// let c = ab % 10;
+// console.log(a,b,c);
+// console.log(b,a,c);
+
+// // INTEGER5
+//  abc = +prompt("uch xonali son kiriting");
+// let ab = abc % 100;
+// let a = (abc - ab) / 100;
+// let b = (abc - a) % 10;
+// let c = ab % 10;
+// console.log(a,b,c);
+// console.log(a,c,b);
+
+// // INTEGER6
+// let abcd = +prompt("to'rt xonali son kiriting");
+// let cd = abcd % 100;
+// let ab = (abcd - cd) / 100;
+// let b = ab % 10;
+// console.log(abcd);
+// console.log(b);
+
+// // INTEGER7
+// let secunds = +prompt("son secunda");
+// let r = secunds % 3600;
+// let secund = r % 60;
+// console.log("daqiqa",secund);
+
+// // INTEGER8
+// let secunds = +prompt("son secunda");
+// let r = secunds % 3600;
+// let hours = (secunds - r) / 3600;
+// console.log(hours ,"soat o'tdi");
+
+// // INTEGER9
+// let secunds = +prompt("son secunda");
+// let r = secunds % 3600;
+// let secund = r%60;
+// let minut = (r - secund) / 60;
+// console.log("daqiqa", secund);
+// console.log("soniya",minut);
+
+// // INTEGER10
+// let secunds = +prompt("son secunda");
+// let r = secunds % 3600;
+// let hours = (secunds - r) / 3600;
+// let secund = r%3600;
+// console.log("daqiqa", hours);
+// console.log("soniya",secund); 
